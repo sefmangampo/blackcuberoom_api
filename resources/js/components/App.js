9de75373db0a8c473b666bdc0fc53b5886a057ed
sef <sefmangampo@gmail.com>
2021-08-15 18:24:50 +0800

    create starting react template with sanctum auth

---
+++ resources/js/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from "react-dom";
+
+export default function App() {
+    return (
+        <div>
+            <h1>Hello worlds</h1>
+        </div>
+    )
+}
+
+if (document.getElementById("main")) {
+    ReactDOM.render(<App />, document.getElementById("main"));
+}
